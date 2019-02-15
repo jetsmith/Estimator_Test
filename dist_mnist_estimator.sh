1a0df8ca7eec9c0785e637352a6aaafe3507d141
jetsmith <m18288748771@163.com>
2019-02-15 16:15:59 +0800

    update

---
+++ dist_mnist_estimator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xe
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export PYTHONPATH=$PYTHONPATH:$DIR
+
+cd $DIR
+# mkdir -p /root/.pip/
+# cp pip.conf /root/.pip/pip.conf
+pip install -r official/requirements.txt
+cd official/mnist
+
+python mnist.py $@
+
+
